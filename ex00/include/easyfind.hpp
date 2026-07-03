8bffadf1535e95ee6b46612b7979d9484605f607
Gautier Ducurtil <gaducurt@student.42.fr>
2026-07-03 14:20:55 +0200

    ex00 works correctly

--- ex00/include/easyfind.hpp
+++ ex00/include/easyfind.hpp
@@ -6,13 +6,21 @@
 /*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/01 15:34:40 by gaducurt          #+#    #+#             */
-/*   Updated: 2026/07/01 15:50:02 by gaducurt         ###   ########.fr       */
+/*   Updated: 2026/07/03 11:56:36 by gaducurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.tpp"
+#include <algorithm>
+#include <iostream>
 
-template <typename T> int easyfind(T a, int b)
+template <typename T>
+void easyfind(T a, int b)
 {
-    
+    typename T::iterator it;
+    it = std::find(a.begin(), a.end(), b);
+    if (it != a.end())
+        std::cout << "The occurrence " << b << " has been found" << std::endl;
+    else
+		throw std::exception();
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,13 +6,60 @@
 /*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/01 15:50:11 by gaducurt          #+#    #+#             */
-/*   Updated: 2026/07/01 15:50:51 by gaducurt         ###   ########.fr       */
+/*   Updated: 2026/07/03 14:19:39 by gaducurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
+#include <vector>
+#include <list>
+#include <iostream>
 
 int main()
 {
-    return (0);
+	std::vector<int>	vect;
+	srand(time(NULL));
+	std::cout << "Contain of vector : ";
+	for (int i = 0; i < 20; i++)
+	{
+		const int value = rand() % 20;
+		vect.push_back(value);
+		std::cout << value;
+		if (i < 19)
+			std::cout << ", ";
+	}
+	std::cout << std::endl;
+	try
+	{
+		const int i = rand() % 10;
+		std::cout << "number to find = " << i << std::endl;
+		easyfind(vect, i);
+	}
+	catch (std::exception & e)
+	{
+		std::cout << "No occurrence has been found." << std::endl;
+	}
+
+	std::list<int>	lst;
+	std::cout << "\nContain of list : ";
+	for (int i = 0; i < 20; i++)
+	{
+		const int value = rand() % 20;
+		lst.push_back(value);
+		std::cout << value;
+		if (i < 19)
+			std::cout << ", ";
+	}
+	std::cout << std::endl;
+	try
+	{
+		const int i = rand() % 10;
+		std::cout << "number to find = " << i << std::endl;
+		easyfind(lst, i);
+	}
+	catch (std::exception & e)
+	{
+		std::cout << "No occurrence has been found." << std::endl;
+	}
+	return (0);
 }
