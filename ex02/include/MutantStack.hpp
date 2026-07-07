bbadb895a3bb5e4bda1fc4aef0cb0343a1c78250
Gautier Ducurtil <gaducurt@student.42.fr>
2026-07-07 17:16:35 +0200

    Mutant

--- ex02/include/MutantStack.hpp
+++ ex02/include/MutantStack.hpp
@@ -6,23 +6,28 @@
 /*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/06 15:51:20 by gaducurt          #+#    #+#             */
-/*   Updated: 2026/07/06 16:11:16 by gaducurt         ###   ########.fr       */
+/*   Updated: 2026/07/07 17:15:30 by gaducurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __MUTANTSTACK__
 #define __MUTANTSTACK__
 
-class MutantStack : 
+#include <stack>
+#include <deque>
+
+template <typename T, class container=std::deque<T> >
+class MutantStack : public std::stack<T, container>
 {
-	private:
-		
 	public:
-		MutantStack();
-		MutantStack(const MutantStack &obj);
-		MutantStack &operator=(const MutantStack &obj);
-		~MutantStack();
-		
+		MutantStack(){}
+		MutantStack(const MutantStack &obj) : std::stack<T, container>(obj){}
+		MutantStack &operator=(const MutantStack &obj){return std::stack<T, container>::operator=(obj);}
+		~MutantStack(){}
+		typedef typename container::iterator iterator;
+
+		iterator			begin(){return this->c.begin();}
+		iterator			end(){return this->c.end();}
 		
 };
 
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,62 @@
 /*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/06 15:51:39 by gaducurt          #+#    #+#             */
-/*   Updated: 2026/07/06 15:51:40 by gaducurt         ###   ########.fr       */
+/*   Updated: 2026/07/07 17:15:45 by gaducurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "MutantStack.hpp"
+
+#include <list>
+
+int main()
+{
+	std::cout << "Exemple with MutantStack :" << std::endl;
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << "top = " << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << "size = " << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+	    std::cout << *it << std::endl;
+	    ++it;
+	}
+	std::stack<int> s(mstack);
+	
+	/*------------------------------------------------------------------------*/
+
+	std::cout << "\nExemple with std::list :" << std::endl;
+	std::list<int> lst;
+	lst.push_back(5);
+	lst.push_back(17);
+	std::cout << "top = " << lst.back() << std::endl;
+	lst.pop_front();
+	std::cout << "size = " << lst.size() << std::endl;
+	lst.push_back(3);
+	lst.push_back(5);
+	lst.push_back(737);
+	lst.push_back(0);
+	std::list<int>::iterator it_l = lst.begin();
+	std::list<int>::iterator ite_l = lst.end();
+	++it_l;
+	--it_l;
+	while (it_l != ite_l)
+	{
+		std::cout << *it_l << std::endl;
+		++it_l;
+	}
+	std::list<int> l(lst);
+	return 0;
+}
