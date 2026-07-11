32f432f780ae5508d463f2b09d3e55eb6d66f9c9
Gautier Ducurtil <gaducurt@student.42.fr>
2026-07-11 12:36:24 +0200

    new tests in ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gaducurt <gaducurt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/03 14:40:52 by gaducurt          #+#    #+#             */
-/*   Updated: 2026/07/07 16:28:40 by gaducurt         ###   ########.fr       */
+/*   Updated: 2026/07/11 12:35:02 by gaducurt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,34 @@
 
 int main()
 {
-	Span sp = Span(10000);
+	Span sp1(10000);
 	int	myarray[] = {10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23};
 	try
 	{
-		sp.insert(myarray, 2, 7);
-		sp.addNumber(6);
-		sp.display();
-		std::cout << "shortest = " << sp.shortestSpan() << std::endl;
-		std::cout << "longest = " << sp.longestSpan() << std::endl;
+		sp1.insert(myarray, 2, 7);
+		sp1.addNumber(6);
+		sp1.display();
+		std::cout << "shortest = " << sp1.shortestSpan() << std::endl;
+		std::cout << "longest = " << sp1.longestSpan() << std::endl;
 	}
 	catch (std::exception &e)
 	{
-		std::cout << "Too much number" << std::endl;
+		std::cout << "Error : Too much number" << std::endl;
+	}
+	std::cout << std::endl;
+	Span	sp2(2);
+	try
+	{
+		sp2.addNumber(1);
+		sp2.addNumber(2);
+		sp2.display();
+		std::cout << "shortest = " << sp2.shortestSpan() << std::endl;
+		std::cout << "longest = " << sp2.longestSpan() << std::endl;
+		sp2.addNumber(3);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Error : Too much number" << std::endl;
 	}
 	return 0;
 }
